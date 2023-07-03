086a0ab13d0dcb7eb5b7ea6470897a489462cf5c
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-03 22:20:40 +0200

    count NAs

--- lesson5.R
+++ lesson5.R
@@ -6,3 +6,11 @@ data()
 data("airquality")
 ??airquality
 #prova sper vedere se push si abilita
+aq = na.omit(airquality)
+head(aq)
+library(dplyr)
+airquality %>% 
+  summarise(across(everything(),~ sum(is.na(.))))
+aq %>% 
+  summarise(across(everything(),~ sum(is.na(.))))
+nrow(airquality)
